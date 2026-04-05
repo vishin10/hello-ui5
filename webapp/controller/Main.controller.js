492f495cdfe2919fd2e3a77153807f8d59767136
vishin10 <vishnuavula5@gmail.com>
2026-04-05 00:07:04 -0400

    Enhance Main.controller.js with dialog functionality and update Main.view.xml to include new buttons; add InfoDialog.fragment.xml for reusable dialog UI components.

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, MessageBox, JSONModel, Fragment) {
     "use strict";
 
     return Controller.extend("hello.ui5.controller.Main", {
@@ -15,10 +16,38 @@ sap.ui.define([
         },
         onPress: function () {
             MessageToast.show("Button clicked!");
-            MessageBox.alert("Hey! You clicked the button.");
+            MessageBox.alert("Hey! You clicked the button.", {
+                icon: MessageBox.Icon.INFORMATION,
+                title: "Alert"
+            });
         },
         onViewFolder: function () {
             MessageToast.show("View folder clicked!");
+            MessageBox.alert("Hey! You clicked View Folder.", {
+                icon: MessageBox.Icon.INFORMATION,
+                title: "Alert"
+            });
+        },
+        onOpenDialog: function () {
+            var oView = this.getView();
+
+            if (!this._pDialog) {
+                this._pDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "hello.ui5.view.fragments.InfoDialog",
+                    controller: this
+                }).then(function (oDialog) {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this._pDialog.then(function (oDialog) {
+                oDialog.open();
+            });
+        },
+        onDialogClose: function (oEvent) {
+            oEvent.getSource().getParent().close();
         }
     });
 });
